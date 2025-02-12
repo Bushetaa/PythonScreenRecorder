7affbfca7b18b5a3c553578191fba720b45d6a9b
sherif thabit ahmed jacoub <109560929+Bushetaa@users.noreply.github.com>
2025-02-13 00:05:27 +0200

    Update ProjectScreenRecord.py

--- ProjectScreenRecord.py
+++ ProjectScreenRecord.py
@@ -345,8 +345,8 @@ class ScreenRecorder:
     
     def _find_ffmpeg(self):
         possible_paths = [
-            r'D:\work\ghayma\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe',
-            r'C:\Windows\System32\ffmpeg',
+            r'D:\ffmpeg.exe',
+            r'C:\ffmpeg',
             os.path.join(os.getcwd(), 'ffmpeg.exe')
         ]
         
